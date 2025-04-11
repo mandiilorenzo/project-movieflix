418f5959f3affe0d49253d947b6ea0800f8b9254
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-04-11 17:10:58 -0300

    adicionada mensagem de erro no cadastro de filmes

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,27 @@ app.get("/movies", async (_, res) => {
     res.json(movies);
 });
 
+app.use(express.json());
+
+app.post("/movies", async (req, res) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date)
+            }
+        });
+    } catch {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
+    }
+    res.status(201).send();
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
