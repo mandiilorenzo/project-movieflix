a769b67208985b945cf9b88567c311f337328581
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-06-18 19:14:03 -0300

    instalação do jwt e criação do loginUser

--- src/services/auth.controller.ts
+++ src/services/auth.controller.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -26,5 +27,33 @@ export const registerUser = async (req: Request, res: Response) => {
         res.status(201).json({ message: "Usuário criado com sucesso", user });
     } catch (error) {
         res.status(500).json({ message: "Erro ao cadastrar usuário", error });
+    }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await prisma.users.findUnique({ where: { email } });
+
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado" });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ message: "Senha incorreta" });
+        }
+
+        const token = jwt.sign(
+            { userId: user.id, role: user.role },
+            process.env.JWT_SECRET || "default_secret", // use .env para isso
+            { expiresIn: "1h" }
+        );
+
+        res.status(200).json({ message: "Login bem-sucedido", token });
+    } catch (error) {
+        res.status(500).json({ message: "Erro ao fazer login", error });
     }
 };
