282dfbd3c8e61606e199d7aee7d340311c35ec62
mandilorenzo <amanda.lorenzo@hotmail.com>
2025-06-18 19:17:16 -0300

    criando a rota de login

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../services/auth.controller";
+import {  registerUser, loginUser } from "../services/auth.controller";
 
 const router = Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
